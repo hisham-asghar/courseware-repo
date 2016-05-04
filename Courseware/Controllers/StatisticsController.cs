fc9937e045ad31a081762db4b7c8dbd2c7a1b279
Hisham Asghar <hisham@octacer.com>
2016-05-05 00:02:15 +0500

    no message

--- Courseware/Controllers/StatisticsController.cs
+++ Courseware/Controllers/StatisticsController.cs
@@ -19,6 +19,10 @@ namespace Courseware.Controllers
         {
             return View();
         }
+        public ActionResult MonthlyStat()
+        {
+            return View();
+        }
 
     }
 }
