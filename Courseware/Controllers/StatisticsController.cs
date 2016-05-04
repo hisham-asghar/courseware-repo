7c57735f45626007e79c6999d17594aaa135fec8
Hisham Asghar <hisham@octacer.com>
2016-05-04 22:21:05 +0500

    no message

---
+++ Courseware/Controllers/StatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Courseware.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        //
+        // GET: /Statistics/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+    }
+}
