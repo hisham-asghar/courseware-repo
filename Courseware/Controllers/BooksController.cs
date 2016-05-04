d373dfd7c423ab54d637ff010694d243ad3e1970
Hisham Asghar <hisham@octacer.com>
2016-05-04 22:11:59 +0500

    no message

---
+++ Courseware/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Courseware.Controllers
+{
+    public class BooksController : Controller
+    {
+        //
+        // GET: /Books/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+         public ActionResult Search()
+        {
+            return View();
+        }
+        public ActionResult SearchByBookName()
+        {
+            return View();
+        }
+        public ActionResult SearchByDepartment()
+        {
+            return View();
+        }
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+
+    }
+
+    
+}
